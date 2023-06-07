e70eead9fd31e23f141c55bec8163a87b04f01c5
ma-2pac <marcoalec@gmail.com>
2023-06-07 15:37:37 +1000

    add bottom section to nmi deets page

--- app/pages/1_nmi.py
+++ app/pages/1_nmi.py
@@ -4,15 +4,19 @@ import pandas as pd
 import sqlite3
 from geopy.geocoders import Nominatim
 import folium
+import plotly.express as px
+from streamlit import SessionState
 
 #global variables
 reading_type =['Select a reading type','Export kWh', 'Import kWh', 'Export kVARh', 'Import kVARh', 'Cost ex GST', 'Carbon kg']
 nmi_list =['Select a NMI','nmi1','nmi2','nmi3']
 
 
+
 # Page: NMI Details
 
 def nmi_page():
+    submit_valid=False
     st.title("NMI Details")
 
     #top  page container
@@ -36,43 +40,80 @@ def nmi_page():
             #validate nmi and reading inputs
             if nmi_in =='Select a NMI' or read_in == 'Select a reading type':
                 st.write('Invalid submission. Try again')
+                submit_valid=False
 
             else:
                 st.write('Valid submission')
+                submit_valid=True
 
-    
-    with st.container():
-        st.header("Display map and nmi deets")
-
-        #setup columns
-        col1, col2 = st.columns(2)
-
-        #temp values
-        address ='83 Mount Street, North Sydney NSW 2060'
-
-        with col1:
-            # Geocode address and display map
-            if address:
-                geolocator = Nominatim(user_agent="my_app")
-                location = geolocator.geocode(address)
-                if location:
-                    latitude, longitude = location.latitude, location.longitude
-                    location_df = pd.DataFrame(data=[[latitude,longitude]],columns=['lat','lon'])
-                    st.map(location_df)
-    
-        customer='test customer'
-
-        with col2:
-            #create details table
-            table_data ={
-                'Detail': ['Master Customer','Site Alias','Customer Classification Code', 'Customer Threshold Code', 'Average Daily Load'],
-                'Value': [customer, 'test site','test code', 'test code','test load']
-            }
-
-            table_df = pd.DataFrame(table_data)
-
-            st.table(table_df)
     #middle page container
+    with st.container():
+        if submit_valid:
+            st.header("Display map and nmi deets")
 
+            #setup columns
+            col1, col2 = st.columns(2)
+
+            #temp values
+            address ='83 Mount Street, North Sydney NSW 2060'
+
+            with col1:
+                # Geocode address and display map
+                if address:
+                    geolocator = Nominatim(user_agent="my_app")
+                    location = geolocator.geocode(address)
+                    if location:
+                        latitude, longitude = location.latitude, location.longitude
+                        location_df = pd.DataFrame(data=[[latitude,longitude]],columns=['lat','lon'])
+                        st.map(location_df)
+        
+            customer='test customer'
+
+            with col2:
+                #create details table
+                table_data ={
+                    'Detail': ['Master Customer','Site Alias','Customer Classification Code', 'Customer Threshold Code', 'Average Daily Load'],
+                    'Value': [customer, 'test site','test code', 'test code','test load']
+                }
+
+                table_df = pd.DataFrame(table_data)
+
+                st.table(table_df)
+
+    #bottom page container
+    with st.container():
+        if submit_valid:
+
+            #setup cols
+            col1, col2 = st.columns(2)
+
+            with col1:
+
+                #responsible party table
+                resp_pty ={
+                    'Party': ['placeholder'],
+                    'Last Role': ['placeholder'],
+                    'Latest From Date': ['placeholder']
+                }
+
+                resp_pty_df = pd.DataFrame(resp_pty)
+
+                st.table(resp_pty_df)
+
+            with col2:
+                
+                #sample data
+                data ={
+                    'date': pd.date_range(start='2023-01-01',end='2023-12-31',freq='D'),
+                    'value': range(365)
+                }
+
+                chart_df = pd.DataFrame(data)
+
+                # Create line chart with Plotly
+                fig = px.line(chart_df, x='date', y='value', title=f'{nmi_in} {read_in}')
+
+                #render fig
+                st.plotly_chart(fig)
 
 nmi_page()
