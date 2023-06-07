12c205859a39d518494aa1f615bd0d6f08e8621f
ma-2pac <marcoalec@gmail.com>
2023-06-07 14:33:06 +1000

    create input section into nmi details page

--- app/pages/1_nmi.py
+++ app/pages/1_nmi.py
@@ -4,8 +4,8 @@ import pandas as pd
 import sqlite3
 
 #global variables
-reading_type =['Export kWh', 'Import kWh', 'Export kVARh', 'Import kVARh', 'Cost ex GST', 'Carbon kg']
-nmi_list =['nmi1','nmi2','nmi3']
+reading_type =['Select a reading type','Export kWh', 'Import kWh', 'Export kVARh', 'Import kVARh', 'Cost ex GST', 'Carbon kg']
+nmi_list =['Select a NMI','nmi1','nmi2','nmi3']
 
 
 # Page: NMI Details
@@ -14,20 +14,33 @@ def nmi_page():
     st.title("NMI Details")
 
     #top  page container
-    with st.beta_container():
+    with st.container():
         st.header('Enter NMI details here')
 
         #set page columns
-        col1, col2 =st.beta_columns(2)
+        col1, col2 =st.columns(2)
 
         with col1:
             nmi_in = st.selectbox("Select a NMI", nmi_list)
             read_in = st.selectbox("Select an option", reading_type)
 
         with col2:
-            start_dt_in = st.text_input("Start Date")
-            end_dt_in = st.text_input("End Date")
+            start_dt_in = st.date_input("Start Date")
+            end_dt_in = st.date_input("End Date")
+
+        #add submit button
+        if st.button("Submit"):
+
+            #validate nmi and reading inputs
+            if nmi_in =='Select a NMI' or read_in == 'Select a reading type':
+                st.write('Invalid submission. Try again')
+
+            else:
+                st.write('Valid submission')
+
     
+    with st.container():
+        st.header("Display map and nmi deets")
     
     #middle page container
 
